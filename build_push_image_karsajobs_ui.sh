581351a30ecee9f21d9387ea9d708a19326ae1a0
Andre Kho <andreyonathan65@gmail.com>
2023-08-24 15:15:44 +0700

    Add pipeline

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -4,10 +4,6 @@
 echo "Membuat image..."
 docker build -t ghcr.io/nekoto-kun/karsajobs-ui:latest .
 
-# Menunggu input PAT
-echo "Masukkan PAT: "
-read -s CR_PAT
-
 # Melakukan login ke GitHub Container Registry
 echo "Melakukan login ke GitHub Container Registry..."
 echo $CR_PAT | docker login ghcr.io -u nekoto-kun --password-stdin
