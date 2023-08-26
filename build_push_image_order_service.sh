07de4b43f3f74578ac85e169987a5e502e563941
Andre Kho <andreyonathan65@gmail.com>
2023-08-26 19:42:38 +0700

    Add pipeline

---
+++ build_push_image_order_service.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Menjalankan build image
+echo "Membuat image..."
+docker build -t ghcr.io/nekoto-kun/order-service:latest .
+
+# Melakukan login ke GitHub Container Registry
+echo "Melakukan login ke GitHub Container Registry..."
+echo $CR_PAT | docker login ghcr.io -u nekoto-kun --password-stdin
+
+# Melakukan push image ke GitHub Container Registry
+echo "Melakukan push image ke GitHub Container Registry..."
+docker push ghcr.io/nekoto-kun/order-service:latest
+
+# Selesai
+echo "Selesai"
